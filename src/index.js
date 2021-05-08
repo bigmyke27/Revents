44ba6f14f628fe776dbe93c6bb3bf0d55e8590ac
Michael Bruce <asamaighobruce27@gmail.com>
2021-05-08 08:31:25 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
+ 
+ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+ 
+const rootEL = document.getElementById('root');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let render = () => {
+  ReactDOM.render(<App />, rootEL)
+}
+
+if (module.hot){
+  module.hot.accept('./App', () => {
+    setTimeout(render);
+  })
+}
+
+render();
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
